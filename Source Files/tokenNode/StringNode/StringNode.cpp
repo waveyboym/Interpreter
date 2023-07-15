d6ddff98af0829b51fa5bdea6a30e3d0ea12c91f
Michael <93211335+waveyboym@users.noreply.github.com>
2023-07-15 18:31:11 +0200

    Delete Source Files directory

--- Source Files/tokenNode/StringNode/StringNode.cpp
+++
@@ -1,5 +0,0 @@
-#include "StringNode.h"
-
-StringNode::StringNode(){}
-
-std::string StringNode::__represent__(){return this->stringValue;}
--- Source Files/tokenNode/tokenNode.cpp
+++
@@ -1,3 +0,0 @@
-#include "tokenNode.h"
-
-tokenNode::tokenNode(){}
